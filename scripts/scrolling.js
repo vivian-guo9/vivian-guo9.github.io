1a767fdfdf8351e746233c675598bddd27b1c150
Vivian Guo <vguo@purdue.edu>
2020-11-13 13:18:19 -0500

    document

--- scripts/scrolling.js
+++ scripts/scrolling.js
@@ -1,7 +1,8 @@
 var w = $(window), d = $(document); 
 w.scroll(function() { 
    if(w.scrollTop() + w.height() == d.height()) 
-   { 
+   {
+	console.log(document) 
       console.log("You have hit the bottom!"); 
    }  
 }); 
