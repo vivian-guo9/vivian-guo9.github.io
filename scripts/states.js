aee21ced0c4dd3827e8996e38505dff6f1210adc
Vivian Guo <vguo@purdue.edu>
2020-11-17 14:57:09 -0500

    trying on https:

--- scripts/states.js
+++ scripts/states.js
@@ -1,17 +1,43 @@
+/*var width = 960;
+var height = 500;
+console.log("script");
+var projection = d3.geoAlbersUsa()
+            .translate([width/2, height/2])
+            .scale([width * 1.36]);
+var us = d3.select("#usmap")
+
+var path = d3.geoPath().projection(projection);
+
+var legendText = ["Republican", "Democratic"];
+var pathgroup = us.append("g")
+            .attr("id", "path");
+
+d3.json("../data/us-states.json", function(json) {
+	pathgroup.selectAll("path")
+	.data(json.features)
+	.enter()
+        .append("path")
+        .attr("d", path)
+        .style("stroke", "#fff")
+	.style("stroke-width", "1");
+});
+*/
+console.log("script2");
+var svg = d3.select("#usmap");
+
 var path = d3.geoPath();
-let usmap = d3.select("#usmap")
 
 d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
   if (error) throw error;
 
-  usmap.append("g")
+  svg.append("g")
       .attr("class", "states")
     .selectAll("path")
     .data(topojson.feature(us, us.objects.states).features)
     .enter().append("path")
       .attr("d", path);
 
-  usmap.append("path")
+  svg.append("path")
       .attr("class", "state-borders")
       .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
 });
